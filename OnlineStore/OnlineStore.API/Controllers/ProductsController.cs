90bbb4f746eed5d344873f863cf4e53041a01663
Ovidiu00 <pernes.ovidiu00@gmail.com>
2022-02-28 10:00:51 +0200

    minor fixes - removed unused namespaces

--- OnlineStore/OnlineStore.API/Controllers/ProductsController.cs
+++ OnlineStore/OnlineStore.API/Controllers/ProductsController.cs
@@ -5,7 +5,6 @@ using OnlineStore.API.ViewModels;
 using OnlineStore.Business.Mediator.Requests.Queries;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace OnlineStore.API.Controllers
--- OnlineStore/OnlineStore.API/Startup.cs
+++ OnlineStore/OnlineStore.API/Startup.cs
@@ -1,23 +1,16 @@
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using OnlineStore.API.AutoMapper;
 using OnlineStore.Business;
 using OnlineStore.Business.BusinessMapper;
 using OnlineStore.DataAccess.Models.AppDbContext;
 using OnlineStore.DataAccess.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace OnlineStore.API
 {
