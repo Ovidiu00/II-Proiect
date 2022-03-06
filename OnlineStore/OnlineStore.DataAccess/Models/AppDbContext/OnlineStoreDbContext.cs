0686a04c7b211bc25bc02243405a525105bb6271
Ovidiu00 <pernes.ovidiu00@gmail.com>
2022-03-06 18:23:42 +0200

    Modified Product Entity

--- OnlineStore/OnlineStore.DataAccess/Models/AppDbContext/OnlineStoreDbContext.cs
+++ OnlineStore/OnlineStore.DataAccess/Models/AppDbContext/OnlineStoreDbContext.cs
@@ -21,7 +21,6 @@ namespace OnlineStore.DataAccess.Models.AppDbContext
         public DbSet<Category> Categories { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Product> Products { get; set; }
-        public DbSet<ProductPhoto> ProductPhotos { get; set; }
 
     }
 }
