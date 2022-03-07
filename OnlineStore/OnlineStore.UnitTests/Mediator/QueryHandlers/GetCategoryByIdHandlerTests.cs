e49e896c4f9d67fad43e896ad3cafeff5850968b
Ovidiu00 <pernes.ovidiu00@gmail.com>
2022-03-07 20:29:45 +0200

    Addedd unit tests for get category by id handler

--- OnlineStore/OnlineStore.UnitTests/Mediator/QueryHandlers/GetCategoryByIdHandlerTests.cs
+++ OnlineStore/OnlineStore.UnitTests/Mediator/QueryHandlers/GetCategoryByIdHandlerTests.cs
@@ -9,7 +9,6 @@ using OnlineStore.DataAccess.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Xunit;
 
