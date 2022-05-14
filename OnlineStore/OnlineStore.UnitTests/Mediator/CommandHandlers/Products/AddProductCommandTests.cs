bd1fa4e6280a20af34b7831725fdcbb616e60847
muresan-levi-30125 <79535254+muresan-levi-30125@users.noreply.github.com>
2022-05-14 14:23:24 +0300

    AddProductComandTests

--- OnlineStore/OnlineStore.UnitTests/Mediator/CommandHandlers/Products/AddProductCommandTests.cs
+++ OnlineStore/OnlineStore.UnitTests/Mediator/CommandHandlers/Products/AddProductCommandTests.cs
@@ -9,19 +9,20 @@ using OnlineStore.Business.Mediator.Requests.Commands;
 using OnlineStore.DataAccess.Models.Entities;
 using OnlineStore.DataAccess.Repositories;
 using System;
+using System.Linq.Expressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
-namespace OnlineStore.UnitTests.Mediator.CommandHandlers.Categories
+
+namespace OnlineStore.UnitTests.Mediator.CommandHandlers.Products
 {
-    public class AddProoductCommandTests
+    public class AddProductCommandTests
     {
         private readonly Mock<IUnitOfWork> mockUnitOfWork;
         private readonly Mock<IMediator> mockMediator;
-
         private readonly AddProductCommandHandler handler;
 
-        public AddProoductCommandTests()
+        public AddProductCommandTests()
         {
             var myProfile = new AutoMapperBussinesProfile();
             var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
@@ -40,14 +41,42 @@ namespace OnlineStore.UnitTests.Mediator.CommandHandlers.Categories
 
             await Assert.ThrowsAsync<Exception>(async () => await handler.Handle(request, new System.Threading.CancellationToken()));
         }
+
         [Fact]
         public async Task AddProductCommand_AllIsValid_SavePhotoCommandIsCalled()
         {
-            AddProductCommand request = new AddProductCommand(new AddProductDTO(),1);
+            AddProductCommand request = new AddProductCommand(new AddProductDTO(), 2);
+            mockUnitOfWork.Setup(w => w.CategoryRepository.FindSingle(It.IsAny<Expression<Func<Category, bool>>>()))
+                        .ReturnsAsync(new Category());
+
+            //Pentru a evita null reference exception la linia 39 din AddProductCommandHandler
             mockUnitOfWork.Setup(w => w.ProductRepository.Add(It.IsAny<Product>()));
+
 
             await handler.Handle(request, It.IsAny<CancellationToken>());
             mockMediator.Verify(x => x.Send(It.IsAny<SavePhotoCommand>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+        [Fact]
+        public async Task AddProductCommand_CategoryFindSingleReturnsNull_ThrowsException()
+        {
+            AddProductCommand request = new AddProductCommand(new AddProductDTO(), 2);
+            mockUnitOfWork.Setup(w => w.CategoryRepository.FindSingle(It.IsAny<Expression<Func<Category, bool>>>()))
+                        .ReturnsAsync(null as Category);
+
+            await Assert.ThrowsAsync<Exception>(async () => await handler.Handle(request, new System.Threading.CancellationToken()));
+        }
+        [Fact]
+        public async Task AddProductCommand_AllIsValid_CommitIsCalled()
+        {
+            AddProductCommand request = new AddProductCommand(new AddProductDTO(), 2);
+            mockUnitOfWork.Setup(w => w.CategoryRepository.FindSingle(It.IsAny<Expression<Func<Category, bool>>>()))
+                                  .ReturnsAsync(new Category());
+
+            //Pentru a evita null reference exception la linia 39 din AddProductCommandHandler
+            mockUnitOfWork.Setup(w => w.ProductRepository.Add(It.IsAny<Product>()));
+
+            await handler.Handle(request, It.IsAny<CancellationToken>());
+            mockUnitOfWork.Verify(x => x.Commit(), Times.Once);
         }
     }
 }
---
+++ OnlineStore/OnlineStore.UnitTests/Mediator/CommandHandlers/Products/EditProductCommandTests.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using OnlineStore.Business.BusinessMapper;
+using OnlineStore.Business.DTOs;
+using OnlineStore.Business.Mediator.Handlers.CommandHandlers;
+using OnlineStore.Business.Mediator.HelperCommands;
+using OnlineStore.Business.Mediator.Requests.Commands;
+using OnlineStore.DataAccess.Models.Entities;
+using OnlineStore.DataAccess.Repositories;
+using System;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OnlineStore.UnitTests.Mediator.CommandHandlers.Categories
+{
+    public class EditProductCommandTests
+    {
+        private readonly Mock<IUnitOfWork> mockUnitOfWork;
+        private readonly Mock<IMediator> mockMediator;
+        private readonly EditProductCommandHandler handler;
+
+        public EditProductCommandTests()
+        {
+            var myProfile = new AutoMapperBussinesProfile();
+            var configuration = new MapperConfiguration(cfg => cfg.AddProfile(myProfile));
+
+            IMapper mapper = new Mapper(configuration);
+            mockUnitOfWork = new Mock<IUnitOfWork>();
+            mockMediator = new Mock<IMediator>();
+
+            handler = new EditProductCommandHandler(mockUnitOfWork.Object, mapper, mockMediator.Object);
+        }
+
+
+    }
+}
